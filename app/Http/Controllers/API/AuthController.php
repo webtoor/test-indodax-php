476ee6afa55f382974af647042b74f2bc5459ce8
webtoor <fityanalimunshi@gmail.com>
2020-09-30 18:59:53 +0700

    auth config

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
 class AuthController extends Controller
@@ -40,6 +41,30 @@ class AuthController extends Controller
     }
 
     public function signIn(Request $request){
-        return "hehehe";
+        $validatedData = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required'
+        ]);
+        $resultUser = User::where('username', $validatedData['username'])->first();
+        if($resultUser) {  
+            if (Hash::check($validatedData['password'], $resultUser->password)) {
+                $request->request->add([
+                    "grant_type" => "password",
+                    "client_id" => "3",
+                    "client_secret" => "tyr3PagBXjolRIahvZagAfPm6mREGfqjQKf0EEfV",
+                    "username"  => $resultUser->email,
+                    "password"  => $validatedData['password'],
+                    "scope"     => "*"
+                ]);
+                $proxy = $request->create('/oauth/token', 'POST');
+                $response = Route::dispatch($proxy);
+
+                $json = (array) json_decode($response->getContent());
+                $json['user_id'] = $resultUser->user_id;
+                $json['email'] = $resultUser->email;
+                $json['username'] = $resultUser->username;
+                return $response->setContent(json_encode($json));
+            }
+        }
     }
 }
