5310156d5953157407afb7daac1c0ea26e6e8921
webtoor <fityanalimunshi@gmail.com>
2020-10-01 17:56:45 +0700

    deploy front-end

--- web/src/app/pages/about/about.page.ts
+++ web/src/app/pages/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
-
-  constructor() { }
+  userData
+  constructor(public menu: MenuController) { 
+  }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.userData = JSON.parse(localStorage.getItem('indodax-laravel'));
+    console.log(this.userData)
+    if(this.userData == null){
+      this.menu.enable(false);
+
+    }else{
+      this.menu.enable(true);
+    }
+    
   }
 
 }
